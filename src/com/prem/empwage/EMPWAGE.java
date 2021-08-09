2c87a919828d64c5dcc86de7aab70a1c9541375a
prem-12345 <premkpatil1997@gmail.com>
2021-08-09 18:24:53 +0530

    UC4_WAGE_FOR_MONTH

--- src/com/prem/empwage/EMPWAGE.java
+++ src/com/prem/empwage/EMPWAGE.java
@@ -1,38 +1,44 @@
 package com.prem.empwage;
 
 /**
- * UC3 ADD PART TIME EMPLOYEE AND WAGE
+ * UC4 SOLVING USING SWITCH CASE STATEMENT CALCULATE WAGES FOR MONTH
  *
  * @author prem
- * @version 1.2
+ * @version 1.3
  * @since 08/09/2021
  */
 
+import java.util.Scanner;
+
 public class EMPWAGE {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         int is_full_time = 1;
-        int is_part_time=2;
+        int is_part_time = 2;
         int is_absent = 0;
 
-        int emp_hr = 16;
+        int full_Time_hr = 16;
+        int part_Time_hr = 8;
         int Wage_per_hr = 20;
+        int days = 20;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == 1) {
-            System.out.println("EMPLOYEE IS FULL_TIME");
-            System.out.println("Emp hour");System.out.println(emp_hr=16);
-        }else if(empCheck==2){
-            System.out.println("EMPLOYEE IS PART_TIME");
-            System.out.println("Emp hour");System.out.println(emp_hr=8);
+        System.out.println("Enter:");
+        int emp_check = sc.nextInt();
+        // System.out.println(emp_check);
 
+        switch (emp_check) {
+            case 0:
+                System.out.println(0);
+                break;
+            case 1:
+                int salary = (Wage_per_hr * full_Time_hr * days);
+                System.out.println(salary);
+                break;
+            case 2:
+                salary = (Wage_per_hr * part_Time_hr * days);
+                System.out.println(salary);
+                break;
         }
-        else{
-            System.out.println("EMPLOYEE IS ABSENT");
-            System.out.println("Emp hour");System.out.println(emp_hr = 0);
-        }
-
-        int dailywage = (emp_hr * Wage_per_hr);
-        System.out.println("Emp Daily Wage");
-        System.out.println(dailywage);
     }
 }
