276f1709b193c76a4fab9386edd9090ffea144b6
prem-12345 <premkpatil1997@gmail.com>
2021-08-09 17:34:20 +0530

    EMP_WAGE_PROGRAMME

---
+++ src/com/prem/empwage/EMPWAGE.java
@@ -0,0 +1,9 @@
+package com.prem.empwage;
+
+//WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME
+
+public class EMPWAGE {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
+    }
+}
