d27ec9650f0475eaf046b05786694297e159b71c
prem-12345 <premkpatil1997@gmail.com>
2021-08-09 17:49:32 +0530

    UC1_CHECK_EMP_PRESENT_ABSENT

--- src/com/prem/empwage/EMPWAGE.java
+++ src/com/prem/empwage/EMPWAGE.java
@@ -1,9 +1,23 @@
 package com.prem.empwage;
 
-//WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME
+/**
+ * UC1 CHECK EMPLOYEE IS PRESENT OR ABSENT
+ *
+ * @author prem
+ * @version 1.0
+ * @since 08/09/2021
+ */
 
 public class EMPWAGE {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
+        int is_full_time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == 1) {
+            System.out.println("EMPLOYEE IS PRESENT");
+        } else {
+            System.out.printf("EMPLOYEE IS ABSENT");
+        }
+
     }
 }
