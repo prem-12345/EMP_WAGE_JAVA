d001b3f5a454bfaad14ef322b0ed94f22b6cde42
prem-12345 <premkpatil1997@gmail.com>
2021-08-09 17:59:14 +0530

    UC2_DAILY_EMP_WAGE

--- src/com/prem/empwage/EMPWAGE.java
+++ src/com/prem/empwage/EMPWAGE.java
@@ -1,23 +1,35 @@
 package com.prem.empwage;
 
 /**
- * UC1 CHECK EMPLOYEE IS PRESENT OR ABSENT
+ * UC2 CALCULATE DAILY EMPLOYEE WAGE
  *
  * @author prem
- * @version 1.0
+ * @version 1.1
  * @since 08/09/2021
  */
 
 public class EMPWAGE {
     public static void main(String[] args) {
         int is_full_time = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int is_absent = 0;
 
+        int emp_hr = 8;
+        int Wage_per_hr = 20;
+
+        double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == 1) {
-            System.out.println("EMPLOYEE IS PRESENT");
+            System.out.println("EMPLOYEE IS FULL_TIME");
+            System.out.println("Emp hour");
+            System.out.println(emp_hr = 8);
         } else {
-            System.out.printf("EMPLOYEE IS ABSENT");
+            System.out.println("EMPLOYEE IS ABSENT");
+            System.out.println("Emp hour");
+            System.out.println(emp_hr = 0);
         }
+
+        int dailywage = (emp_hr * Wage_per_hr);
+        System.out.println("Emp Daily Wage");
+        System.out.println(dailywage);
 
     }
 }
